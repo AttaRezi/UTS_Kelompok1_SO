25b149701636dc4f0fbf9ec63e5da184a4954ecf
AttaRezi <100833968+AttaRezi@users.noreply.github.com>
2022-04-07 09:45:46 +0700

    Update mmap.c

--- mmap.c
+++ mmap.c
@@ -3,8 +3,7 @@
 #include <unistd.h>     /* fork() and getpid() */
 #include <stdio.h>      /* printf() */
 #include <sys/wait.h>   /*wait*/
-#include <errno.h>	    /* errno */
-#include <stdio.h>
+#include <errno.h>      /* errno */
 #include <string.h>
 
 #define MSGSIZE 3
@@ -16,21 +15,26 @@ int main(int argc, char **argv) {
         //buat memory map
         void *addr1 = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
         void *addr2 = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-        printf("Mapped at %p\n", addr1);
+    
         int *shared1 = addr1;
         char *shared2 = addr2;
-        int temp1[100];
-        int temp2 = 0; //temp2 untuk menampung nilai penjumlahan dari keseluruhan nilai temp1
-        char temp3[100];
+        printf("Shared1 Mapped at %p\n", addr1);
+        printf("Shared2 Mapped at %p\n", addr2);
+        
+        int temp1[100]; //data
+        int temp2 = 0; //total data
         for(i=0; i<MSGSIZE; i++){
+            printf("INPUT DATA ");
             scanf("%d", &temp1[i]);
             temp2 = temp2 + temp1[i];
         }
+        
+        char temp3[100];
         sprintf(temp3,"%d", temp2);
+        
+        
         switch (pid = fork()) {
         case 0:         /*  proses anak */
-
-            //ketika proses parent dijalankan, disaat yg sama proses child juga berjalan
                 printf("Proses anak \n");
                 //isi data di memory map
                 for(i=0; i<MSGSIZE; i++){
